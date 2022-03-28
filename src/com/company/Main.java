e462e47ece79a455eb656a8a6408b468a0bc8a3b
augustwiden <widenaugust@gmail.com>
2022-03-28 10:17:44 +0200

    ny commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("ojjjdå");
         System.out.println("2");
+        System.out.println("jhfiahjfjwaf");
     }
 }
